9c74500606a3084bde0aef8bad5bd1e515fdc4c4
juicyfruityo <pooploors@gmail.com>
2020-04-12 14:49:55 +0300

    add docstrings to WeatherManager

--- WeatherManager.py
+++ WeatherManager.py
@@ -7,6 +7,11 @@ class WeatherManager:
         self.token = 'c96e8a61e1996f41d122a69312593c11'
 
     def __get_weather_now(self, city):
+        '''
+        Sending get request to openweathermap.
+        :param city: name of the city to get request
+        :return: result of get request
+        '''
         token = self.token
         params = {'q': city, 'appid': token,
                   'lang': 'ru', 'units': 'metric'}
@@ -15,6 +20,11 @@ class WeatherManager:
         return weather_result
 
     def __convert_result(self, weather_result):
+        '''
+        Converting result of request to understandable format.
+        :param weather_result: json of get request
+        :return: dict with weather condition
+        '''
         temp = weather_result['main']['temp']
         descr = weather_result['weather'][0]['description']
         feels_like = weather_result['main']['feels_like']
@@ -26,6 +36,11 @@ class WeatherManager:
                 'pressure': pressure, 'humidity': humidity, 'wind': wind}
 
     def get_weather(self, city, when='now'):
+        '''
+        :param city: name of the city to get weather
+        :param when: if 'now' then will be processing current weather
+        :return: code of request, resulting dict or error message
+        '''
         if when == 'now':
             weather_result = self.__get_weather_now(city)
         else:
